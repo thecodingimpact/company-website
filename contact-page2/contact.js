ca73b4c8b9c5b3df571d08a72283f3a309df9047
thecodingimpact <157344711+thecodingimpact@users.noreply.github.com>
2024-01-22 23:35:22 +0500

    Update contact.js

--- contact-page2/contact.js
+++ contact-page2/contact.js
@@ -1,31 +1,33 @@
-// Adding focus and blur event listeners to input fields
-const inputs = document.querySelectorAll('.contact__input');
+const inputs = document.querySelectorAll('.contact__input')
 
 inputs.forEach(ipt => {
-  ipt.addEventListener("focus", () => {
-    ipt.parentNode.classList.add("focus");
-    ipt.parentNode.classList.add("not-empty");
-  });
-  ipt.addEventListener("blur", () => {
-    if (ipt.value === "") {
-      ipt.parentNode.classList.remove("not-empty");
-    }
-    ipt.parentNode.classList.remove("focus");
-  });
-});
+    ipt.addEventListener("focus", () => {
+        ipt.parentNode.classList.add("focus")
+        ipt.parentNode.classList.add("not-empty")
+    })
+    ipt.addEventListener("blur", () => {
+        if (ipt.value == "") {
+            ipt.parentNode.classList.remove("not-empty")
+        }
 
-// Toggle dark mode and transition effect
-const toggleBtn = document.querySelector(".theme-toggle");
-const allElements = document.querySelectorAll("*");
+        ipt.parentNode.classList.remove("focus")
+    })
+})
+
+
+// -------------------------color changing bg------------------------------------
+
+const toggleBtn = document.querySelector(".theme-toggle")
+const allElements = document.querySelectorAll("*")
+
 
 toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark")
 
-  allElements.forEach(el => {
-    el.classList.add('transition');
-    setTimeout(() => {
-      el.classList.remove("transition");
-    }, 1000);
-  });
-});
-
+allElements.forEach(el => {
+    el.classList.add('transition')
+    setTimeout(() =>{
+        el.classList.remove("transition")
+    }, 1000)
+})
+})
