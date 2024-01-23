518b415d80efb4ba6dcf640659abd3f6a7c5eef9
thecodingimpact <157344711+thecodingimpact@users.noreply.github.com>
2024-01-23 21:30:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,63 @@ function open_img(evt, cityname) {
   document.getElementById(cityname).style.display = "block";
   evt.currentTarget.classList.add("active");
 }
+         // Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMSGsbkHALvkiS4mcBdRmx__ybdz_pA18",
+  authDomain: "chat-app-bf22d.firebaseapp.com",
+  databaseURL: "https://chat-app-bf22d-default-rtdb.firebaseio.com",
+  projectId: "chat-app-bf22d",
+  storageBucket: "chat-app-bf22d.appspot.com",
+  messagingSenderId: "961267170800",
+  appId: "1:961267170800:web:e6a72daf5d7ed80cb9b135"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database
+const database = firebase.database();
+
+// Get the form element
+const form = document.querySelector('#footer-form');
+
+
+// Add submit event listener
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  // Get input field values
+  const mail = document.querySelector('#mail').value;
+
+  // Save data to Firebase
+  database.ref('menu').push({
+     mail
+  });
+
+
+  // Initialize Email.js with the Public Key
+  emailjs.init("RlxjAgahjJR2mPZ92");
+
+  // Prepare email template parameters
+  const templateParams = {
+    to_email: 'furqancoder1@gmail.com',
+    from_name: 'teapack534@gmail.com',
+    subject: 'Form Submission',
+    message: `Mail: ${mail}`
+  };
+
+  // Define your Email.js service ID and template ID
+  const emailjsServiceId = 'service_4qs7xct';
+  const emailjsTemplateId = 'template_5ufavk6';
+
+  // Send email using Email.js
+  emailjs.send(emailjsServiceId, emailjsTemplateId, templateParams)
+    .then((response) => {
+      console.log('Email sent successfully:', response);
+            alert('You are successfully added in our Mailing List');
+      location.reload();
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error);
+    });
+});
