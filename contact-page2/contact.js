002d4970b3682e46059e5a0c525b16cfe90ad713
thecodingimpact <157344711+thecodingimpact@users.noreply.github.com>
2024-01-26 22:37:17 +0500

    Update contact.js

--- contact-page2/contact.js
+++ contact-page2/contact.js
@@ -19,6 +19,24 @@ function handleInputEvents(ipt) {
   });
 }
 
+
+const inputs = document.querySelectorAll('.contact__input')
+
+inputs.forEach(ipt => {
+    ipt.addEventListener("focus", () => {
+        ipt.parentNode.classList.add("focus")
+        ipt.parentNode.classList.add("not-empty")
+    })
+    ipt.addEventListener("blur", () => {
+        if (ipt.value == "") {
+            ipt.parentNode.classList.remove("not-empty")
+        }
+
+        ipt.parentNode.classList.remove("focus")
+    })
+})
+
+
 // Color-changing background
 const toggleBtn = document.querySelector(".theme-toggle");
 const allElements = document.querySelectorAll("*");
