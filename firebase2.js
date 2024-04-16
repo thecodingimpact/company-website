6306f4a5ddb5dc054c00b3d5c935d37477db0366
thecodingimpact <157344711+thecodingimpact@users.noreply.github.com>
2024-04-16 12:14:51 +0500

    Add files via upload

---
+++ firebase2.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD6vrwfY9fNKAD9GeY0fjbT1de_RuZPlng",
+    authDomain: "salancer-444c7.firebaseapp.com",
+    databaseURL: "https://salancer-444c7-default-rtdb.firebaseio.com",
+    projectId: "salancer-444c7",
+    storageBucket: "salancer-444c7.appspot.com",
+    messagingSenderId: "238896914138",
+    appId: "1:238896914138:web:ff1750cd130c115a7b4fe3",
+    measurementId: "G-FEJ4F9VL2P"
+  };
+  firebase.initializeApp(firebaseConfig);
